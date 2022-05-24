40934be9b9ee0ae5c1dee4b6089b2fd4e3ec58ff
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-05-24 21:48:47 +0500

    booking api addad

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const cors = require ('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.22goc.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+      try{
+          await client.connect();
+          const serviceCollection = client.db('manufacture_portal').collection('services');
+          const bookingCollection = client.db('manufacture_portal').collection('bookings');
+
+          app.get('/service', async(req, res) =>{
+              const query = {};
+              const cursor = serviceCollection.find(query);
+              const services = await cursor.toArray();
+              res.send(services);
+          });
+
+          app.post('/booking', async(req, res) =>{
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+          })
+          
+      }
+      finally{
+          
+      }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello Man!')
