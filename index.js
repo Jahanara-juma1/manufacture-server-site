20a37e5d6c99898db863503b09c0d8ec785614d3
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-05-25 21:31:01 +0500

    JET Token add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require ('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
@@ -11,6 +12,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.22goc.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'UnAuthorization access'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    if(err){
+      return res.status(403).send({message: 'Forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run(){
       try{
@@ -35,7 +51,8 @@ async function run(){
               $set: user,
              };
              const result = await userCollection.updateOne(filter, updateDoc, options);
-             res.send(result);
+             const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+             res.send({result, token});
           })
            //-----------------
           app.get('/available', async(req, res) =>{
@@ -48,11 +65,18 @@ async function run(){
           })
 
           
-          app.get('/booking', async(req, res) =>{
+          app.get('/booking', verifyJWT, async(req, res) =>{
             const client = req.query.client;
-           const query = {client: client};
-            const bookings = await bookingCollection.find(query).toArray();
-            res.send(bookings);
+            const decodedEmail = req.decoded.email;
+            if(client=== decodedEmail){
+              const query = {client: client};
+              const bookings = await bookingCollection.find(query).toArray();
+             return res.send(bookings);
+            }
+            else{
+              return res.status(403).send({message: 'forbidden access'});
+            }
+           
           })
         
           app.post('/booking', async(req, res) =>{
