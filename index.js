0cce2a0b92ae90ed222bdf329f4148aedaed86f1
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-05-25 18:31:42 +0500

    user email addad

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function run(){
           await client.connect();
           const serviceCollection = client.db('manufacture_portal').collection('services');
           const bookingCollection = client.db('manufacture_portal').collection('bookings');
+          const userCollection = client.db('manufacture_portal').collection('users');
 
           app.get('/service', async(req, res) =>{
               const query = {};
@@ -24,7 +25,19 @@ async function run(){
               const services = await cursor.toArray();
               res.send(services);
           });
-
+        //------------------
+          app.put('/user/:email', async(req, res) =>{
+            const email = req.params.email;
+            const user = req.body;
+            const filter = {email: email};
+            const options = { upsert: true };
+            const updateDoc ={
+              $set: user,
+             };
+             const result = await userCollection.updateOne(filter, updateDoc, options);
+             res.send(result);
+          })
+           //-----------------
           app.get('/available', async(req, res) =>{
             const name = req.query.name || 'name';
             //step-1
@@ -34,14 +47,14 @@ async function run(){
             res.send(services);
           })
 
-          //---------------
+          
           app.get('/booking', async(req, res) =>{
             const client = req.query.client;
            const query = {client: client};
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings);
           })
-          //-----------------
+        
           app.post('/booking', async(req, res) =>{
             const booking = req.body;
             const query = {product: booking.product, client: booking.client }
